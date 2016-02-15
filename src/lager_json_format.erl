466fd93dab2e9fe0a8c518693da33db84bd251ee
chenzhaoyu <chenzhaoyu@github.com>
2016-02-15 12:00:59 +0900

    *jiffy -> mochijson2

--- src/lager_json_format.erl
+++ src/lager_json_format.erl
@@ -7,5 +7,29 @@ format(Body, ParsList)->
 format(ParsList) ->
     jsonFormat(ParsList).
 
+json_encode(Term) ->
+    try
+        {ok, mochijson2:encode(Term)}
+    catch
+        exit:{json_encode, E} ->
+            {error, E}
+    end.
+
+json_decode(Term) ->
+    try
+        {ok, mochijson2:decode(Term)}
+    catch
+        %% Sadly `mochijson2:decode/1' does not offer a nice way to catch
+        %% decoding errors...
+        error:_ -> error
+    end.
+
 jsonFormat(ParsList) ->
-    jiffy:encode(ParsList).
+    case json_encode(ParsList) of
+        {error, E} ->
+            io:format("~p", [E]),
+            "";
+        {ok, Ret} ->
+            Ret
+    end.
+
